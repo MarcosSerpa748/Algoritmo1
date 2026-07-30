6512b1fbd36dcd02ea88bba27b1ae2ea67a64aad
marcos <msrserpa@gmail.com>
2026-07-30 15:59:20 -0300

    Cuida.

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,5 @@ public class Main {
       String frase = sc.nextLine();
 
       System.out.println("A quantida de vogais dessa frase é "+Metodos.quantidadeVogais(frase));
-      System.out.println("Teste");
     }
 }
